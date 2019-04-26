1112b6e52f27cdbfacdbfef467d649fe1913701d
Franck Genius de Clerck <franck.de-clerck@pole-emploi.fr>
2019-04-26 16:58:56 +0200

    first commit

---
+++ component---src-pages-about-js-1da2bf863b21a93c56e3.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{141:function(e,t,a){"use strict";a.r(t);var n=a(0),r=a.n(n),c=a(145);t.default=function(){return r.a.createElement("div",{style:{color:"teal"}},r.a.createElement(c.a,{headerText:"fdeclerck.github.io"}),r.a.createElement(c.a,{headerText:"Personal GitHub website"}),r.a.createElement("p",null,"Such wow. Very React."))}},145:function(e,t,a){"use strict";var n=a(0),r=a.n(n);t.a=function(e){return r.a.createElement("h1",null,e.headerText)}}}]);
+//# sourceMappingURL=component---src-pages-about-js-1da2bf863b21a93c56e3.js.map
